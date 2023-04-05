acf0bd6e8a19b5c46b15849b28054c4660cbf244
Daniel Hoskin <ac122731@avcol.school.nz>
2023-04-06 11:03:03 +1200

    Making Last touch up changes ( ps need to make it so when the answer is invaild Redo the Question instead of going onto the next message and it stopping the quiz

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class MainClass
         Console.WriteLine("Welcome to the Te Reo Maori Quiz!");
 
         // Ask for name
-        Console.Write("What is your name? ");
+        Console.Write("What is your name?  \n ");
         string name = Console.ReadLine();
 
         // Reply With hello + name then delay
@@ -19,7 +19,7 @@ class MainClass
         Console.Clear();
 
         // Let the user know that That all answers should be in captials letter for the start of each sentence or A noun or name
-        Console.WriteLine("Please note that all answers should have a captial letter for the start of a answer eg. (Like this) not (not like this)\n But if it is a name please enter there last and first name with a capital letter");
+        Console.WriteLine("Please note that all answers should have a captial letter for the start of a answer eg. (Like this) not (not like this)\nBut if it is a name please enter there last and first name with a capital letter");
         Thread.Sleep(5000);
         Console.Clear();
 
@@ -41,7 +41,7 @@ class MainClass
             case "B":
                 questions = new string[,] {
           {"Hello", "Kia ora"},
-          {"Thank you", "teno koe"},
+          {"Thank you", "Teno koe"},
           {"Goodbye", "Kia ora"},
           {"Yes", "Ae"},
           {"No", "Kao"},
@@ -50,22 +50,22 @@ class MainClass
                 break;
             case "I":
                 questions = new string[,] {
-          {"What is your name?", "Ko wai to ingoa?"},
-          {"Where are you from?", "Nō hea koe?"},
-          {"How are you?", "Kei te pehea koe?"},
-          {"What time is it?", "He aha te taima?"},
-          {"What is this?", "He aha tēnei?"},
-          {"How old are you?", "E hia ou tau?"},
+          {"What is your name", "Ko wai to ingoa?"},
+          {"Where are you from", "Nō hea koe?"},
+          {"How are you", "Kei te pehea koe?"},
+          {"What time is it", "He aha te taima?"},
+          {"What is this", "He aha tēnei?"},
+          {"How old are you", "E hia ou tau?"},
         };
                 break;
             case "H":
                 questions = new string[,] {
-          {"What is the Maori name for New Zealand?", "Aotearoa"},
-          {"What is the name of the longest river in New Zealand?", "Waikato River"},
-          {"Who is the current Prime Minister of New Zealand?", "Chirs Hipkins"},
-          {"What is the name of the famous Maori haka?", "Ka Mate"},
-          {"Who was the first Maori astronaut to go to space?", "Mana Vautier"},
-          {"What is the Maori word for love?", "Aroha"},
+          {"What is the Maori name for New Zealand", "Aotearoa"},
+          {"What is the name of the longest river in New Zealand", "Waikato River"},
+          {"Who is the current Prime Minister of New Zealand", "Chirs Hipkins"},
+          {"What is the name of the famous Maori haka", "Ka Mate"},
+          {"Who was the first Maori astronaut to go to space", "Mana Vautier"},
+          {"What is the Maori word for love", "Aroha"},
         };
                 break;
             default:
@@ -77,7 +77,7 @@ class MainClass
         for (int i = 0; i < questions.GetLength(0); i++)
         {
             // Ask question
-            Console.Write(name + ", what is the Maori translation for '" + questions[i, 0] + "'? ");
+            Console.Write(name + ", what is the Maori translation for '" + questions[i, 0] + "? ");
             string answer = Console.ReadLine();
 
             // Validate answer
@@ -86,7 +86,7 @@ class MainClass
                 Console.WriteLine("Correct!");
                 score++;
             }
-            else if (answer == "A" || answer == "B" || answer == "C")
+            else if (answer == "" || answer == "" || answer == "")
             {
                 Console.WriteLine("Invalid response. Please provide a valid answer.");
                 i--;
@@ -97,7 +97,7 @@ class MainClass
             }
 
             // Delay and clear screen
-            Thread.Sleep(1500);
+            Thread.Sleep(3000);
             Console.Clear();
         }
 
