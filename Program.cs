3146207f38fc5980c4d09d0cf91fe98d7c0f2818
Daniel Hoskin <ac122731@avcol.school.nz>
2023-05-25 13:19:08 +1200

    Fixing and adding methods

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Threading;
-
-class MainClass
+﻿class MainClass
 {
-    public static void Main(string[] args)
+    public static object Main(string[] args)
     {
 
         // Welcome message
@@ -29,80 +26,103 @@ class MainClass
         Console.WriteLine("I for Intermediate");
         Console.WriteLine("H for Hard");
         Console.Write("Your choice: ");
-        string level = Console.ReadLine().ToUpper();
-
-        // Initialize score
+        char level = Console.ReadLine().ToUpper()[0];
+        string[] questions;
         int score = 0;
 
-        // Determine quiz questions based on level
-        string[,] questions;
         switch (level)
         {
-            case "B":
-                questions = new string[,] {
-          {"Hello", "Kia ora"},
-          {"Thank you", "Teno koe"},
-          {"Goodbye", "Kia ora"},
-          {"Yes", "Ae"},
-          {"No", "Kao"},
-          {"I love you", "Aroha ana ahau ki a koe"}
-        };
+            case 'B':
+                askquestions(name, level);
                 break;
-            case "I":
-                questions = new string[,] {
-          {"What is your name", "Ko wai to ingoa?"},
-          {"Where are you from", "Nō hea koe?"},
-          {"How are you", "Kei te pehea koe?"},
-          {"What time is it", "He aha te taima?"},
-          {"What is this", "He aha tēnei?"},
-          {"How old are you", "E hia ou tau?"},
-        };
+
+            case 'I':
+                askquestions(name, level);
                 break;
-            case "H":
-                questions = new string[,] {
-          {"What is the Maori name for New Zealand", "Aotearoa"},
-          {"What is the name of the longest river in New Zealand", "Waikato River"},
-          {"Who is the current Prime Minister of New Zealand", "Chirs Hipkins"},
-          {"What is the name of the famous Maori haka", "Ka Mate"},
-          {"Who was the first Maori astronaut to go to space", "Mana Vautier"},
-          {"What is the Maori word for love", "Aroha"},
-        };
+
+            case 'H':
+                askquestions(name, level);
                 break;
-            default:
-                Console.WriteLine("Invalid level selection. Please select B, I, or H.");
-                return;
         }
 
-        // Ask questions
-        for (int i = 0; i < questions.GetLength(0); i++)
+
+
+        static void askquestions(string name, char level)
         {
-            // Ask question
-            Console.Write(name + ", what is the Maori translation for '" + questions[i, 0] + "? ");
-            string answer = Console.ReadLine();
+            {
 
-            // Validate answer
-            if (answer == questions[i, 1])
-            {
-                Console.WriteLine("Correct!");
-                score++;
+                // Determine quiz questions based on level
+                string[,] Questions;
+                switch (level)
+                {
+                    case "B":
+                        Questions = new string[,] {
+          { "Hello", "Kia ora" },
+          { "Thank you", "Teno koe" },
+          { "Goodbye", "Kia ora" },
+          { "Yes", "Ae" },
+          { "No", "Kao" },
+          { "I love you", "Aroha ana ahau ki a koe" }
+                        };
+                        break;
+                    case "I":
+                        Questions = new string[,] {
+          { "What is your name", "Ko wai to ingoa?" },
+          { "Where are you from", "No hea koe?" },
+          { "How are you", "Kei te pehea koe?" },
+          { "What time is it", "He aha te taima?" },
+          { "What is this", "He aha tenei?" },
+          { "How old are you", "E hia ou tau?" },
+                        };
+                        break;
+                    case "H":
+                        Questions = new string[,] {
+          { "What is the Maori name for New Zealand", "Aotearoa" },
+          { "What is the name of the longest river in New Zealand", "Waikato River" },
+          { "Who is the current Prime Minister of New Zealand", "Chirs Hipkins" },
+          { "What is the name of the famous Maori haka", "Ka Mate" },
+          { "Who was the first Maori astronaut to go to space", "Mana Vautier" },
+          { "What is the Maori word for love", "Aroha" },
+                        };
+                        break;
+                    default:
+                        Console.WriteLine("Invalid level selection. Please select B, I, or H.");
+                        return;
+
+                }
+
             }
-            else if (answer == "" || answer == "" || answer == "")
+            // Ask questions
+            for (int i = 0; i < questions.GetLength(0); i++)
             {
-                Console.WriteLine("Invalid response. Please provide a valid answer.");
-                i--;
-            }
-            else
-            {
-                Console.WriteLine("Incorrect. The correct answer is: " + questions[i, 1]);
+                // Ask question
+                Console.Write(name + ", what is the Maori translation for '" + questions[i, 0] + "? ");
+                string answer = Console.ReadLine();
+
+                // Validate answer
+                if (answer == questions[i, 1])
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else if (answer == "" || answer == "" || answer == "")
+                {
+                    Console.WriteLine("Invalid response. Please provide a valid answer.");
+                    i--;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect. The correct answer is: " + questions[i, 1]);
+                }
+
+                // Delay and clear screen
+                Thread.Sleep(3000);
+                Console.Clear();
             }
 
-            // Delay and clear screen
-            Thread.Sleep(3000);
-            Console.Clear();
+            // Display final score
+            Console.WriteLine("Congratulations " + name + ", you finished the quiz!");
+            Console.WriteLine("Your final score is: " + score + "/" + questions.GetLength(0));
         }
-
-        // Display final score
-        Console.WriteLine("Congratulations " + name + ", you finished the quiz!");
-        Console.WriteLine("Your final score is: " + score + "/" + questions.GetLength(0));
     }
 }
